a5b74cf425a8eb6f9d6f8a4ba373addb5f7933a8
Kristian Nedrevold-Hansen <kristian@krined.no>
2025-01-16 22:06:47 +0100

    Updated bleep to version 0.0.12

--- flake.nix
+++ flake.nix
@@ -6,25 +6,38 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage = let
-        pkgs = import nixpkgs { inherit system; };
-        version = "0.0.3";  # Define the version here
-        suffix = if pkgs.lib.strings.hasInfix "darwin" system then "-apple-darwin" else "-pc-linux";
-        in pkgs.stdenv.mkDerivation rec {
+      defaultPackage =
+        let
+          pkgs = import nixpkgs { inherit system; };
+          version = "0.0.12"; # Define the version here
+          suffix = if pkgs.lib.strings.hasInfix "darwin" system then "-apple-darwin" else "-pc-linux";
+        in
+        pkgs.stdenv.mkDerivation rec {
           name = "bleep-${version}";
 
-          nativeBuildInputs = [ pkgs.installShellFiles pkgs.makeWrapper ]
-            ++ pkgs.lib.optional pkgs.stdenv.isLinux pkgs.autoPatchelfHook;
+          nativeBuildInputs = [
+            pkgs.installShellFiles
+            pkgs.makeWrapper
+          ] ++ pkgs.lib.optional pkgs.stdenv.isLinux pkgs.autoPatchelfHook;
 
-          buildInputs = [ pkgs.glibc pkgs.zlib pkgs.stdenv.cc.cc ];
+          buildInputs = [
+            pkgs.glibc
+            pkgs.zlib
+            pkgs.stdenv.cc.cc
+          ];
 
-          src =
-            pkgs.fetchurl {
-              url = "https://github.com/oyvindberg/bleep/releases/download/v${version}/bleep-x86_64${suffix}.tar.gz";
-              sha256 = "sha256-TbE5D9xHmgskchSj2sT3hTSAJnLsq9ecIEPbiVjdKqQ=";
-            };
+          src = pkgs.fetchurl {
+            url = "https://github.com/oyvindberg/bleep/releases/download/v${version}/bleep-x86_64${suffix}.tar.gz";
+            sha256 = "sha256-sDmqEE8LP+KJ18XjHuNzoQMiTXYu5cLEeiqPU2yj0M4=";
+          };
 
           unpackPhase = ''
             runHook preUnpack
@@ -42,17 +55,19 @@
 
           dontAutoPatchelf = true;
 
-          postFixup = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
-            autoPatchelf $out
-          '' + ''
-            mkdir temp
-            cp $out/bin/.bleep-wrapped temp/bleep
-            PATH="./temp:$PATH"
+          postFixup =
+            pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+              autoPatchelf $out
+            ''
+            + ''
+              mkdir temp
+              cp $out/bin/.bleep-wrapped temp/bleep
+              PATH="./temp:$PATH"
 
-            installShellCompletion --cmd bleep \
-              --bash <(bleep install-tab-completions-bash) \
-              --zsh <(bleep install-tab-completions-zsh) \
-          '';
+              installShellCompletion --cmd bleep \
+                --bash <(bleep install-tab-completions-bash) \
+                --zsh <(bleep install-tab-completions-zsh) \
+            '';
 
           meta = with pkgs.lib; {
             homepage = "https://bleep.build";
